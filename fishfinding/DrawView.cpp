3c8aa1c3001efbb15edbc6c11f21166b404350ae
t1238142000 <t1238142000@15cd899a-37be-affb-661b-94829f8fb904>
2010-12-10 13:55:49 +0000

    Good version

--- fishfinding/DrawView.cpp
+++ fishfinding/DrawView.cpp
@@ -1,7 +1,54 @@
+#include "StdVtkWx.h"
 #include "DrawView.h"
 
 DrawView::DrawView()
 {
+	m_focus_height = 3;
+	vtkSmartNew_Initialize(m_hs_points);
+	vtkSmartNew_Initialize(m_depth_points);
+	vtkSmartNew_Initialize(m_hs_vertices);
+	vtkSmartNew_Initialize(m_depth_vertices);
+	vtkSmartNew_Initialize(m_imagedata);
+	vtkSmartNew_Initialize(m_hs_poly);
+	vtkSmartNew_Initialize(m_depth_poly);
+	vtkSmartNew(m_hs_Mapper);
+	vtkSmartNew(m_depth_Mapper);
+	vtkSmartNew(m_hs_Actor);
+	vtkSmartNew(m_depth_Actor);
+	vtkSmartNew(m_Renderer);
+	vtkSmartNew(m_RenderWindow);
+	vtkSmartNew(m_Camera);
+	vtkSmartNew(m_Axes);
+	vtkSmartNew(m_Append_hs);
+	vtkSmartNew(m_Append_depth);
+	vtkSmartNew(m_WindowInteractor);
+	vtkSmartNew(m_Axes_widget);
+
+	m_RenderWindow->AddRenderer(m_Renderer);
+	m_WindowInteractor->SetRenderWindow(m_RenderWindow);
+	m_Renderer->SetActiveCamera(m_Camera);
+	m_Renderer->SetBackground(.1, .2, .3);
+	m_Axes_widget->SetOutlineColor( 0.8300, 0.6700, 0.5300 );
+	m_Axes_widget->SetOrientationMarker( m_Axes );
+	m_Axes_widget->SetInteractor( m_WindowInteractor );
+	m_Axes_widget->On();
+	// horizontal surface vertex
+	m_hs_poly->SetPoints(m_hs_points);
+	m_hs_poly->SetVerts(m_hs_vertices);
+	m_Append_hs->SetInput(m_hs_poly);
+	m_hs_Mapper->SetInputConnection(m_Append_hs->GetOutputPort());
+	m_hs_Actor->SetMapper(m_hs_Mapper);
+	m_Renderer->AddActor(m_hs_Actor);
+	// depth surface vertex
+	m_depth_poly->SetPoints(m_depth_points);
+	m_depth_poly->SetVerts(m_depth_vertices);
+	m_Append_depth->SetInput(m_depth_poly);
+	m_depth_Mapper->SetInputConnection(m_Append_depth->GetOutputPort());
+	m_depth_Actor->SetMapper(m_depth_Mapper);
+	//m_depth_Actor->GetProperty()->SetPointSize(10);
+	m_Renderer->AddActor(m_depth_Actor);
+
+	
 }
 
 void DrawView::AddDataList( const nmeaINFOs& infos )
@@ -12,8 +59,17 @@ void DrawView::AddDataList( const nmeaINFOs& infos )
 		DataPoint data;
 		data.E = it->lon;
 		data.N = it->lat;
-		data.Depth = it->depthinfo.depth_M;
-		m_points.push_back(data);
+		data.depth = it->depthinfo.depth_M;
+		m_raw_points.push_back(data);
+		const float phs[3] = {data.E, data.N, 0};
+		const float pdep[3] = {data.E, data.N, -data.depth};
+		vtkIdType pid[1];
+		pid[0] = m_hs_points->InsertNextPoint(phs);
+		m_hs_vertices->InsertNextCell(1,pid);
+		m_Append_hs->SetInput(m_hs_poly);
+		pid[0] = m_depth_points->InsertNextPoint(pdep);
+		m_depth_vertices->InsertNextCell(1,pid);
+		m_Append_depth->SetInput(m_depth_poly);
 	}
 }
 
@@ -22,17 +78,96 @@ void DrawView::AddData( const nmeaINFO& info )
 	DataPoint data;
 	data.E = info.lon;
 	data.N = info.lat;
-	data.Depth = info.depthinfo.depth_M;
-	m_points.push_back(data);
+	data.depth = info.depthinfo.depth_M;
+	m_raw_points.push_back(data);
+	const float phs[3] = {data.E, data.N, 0};
+	const float pdep[3] = {data.E, data.N, -data.depth};
+	vtkIdType pid[1];
+	pid[0] = m_hs_points->InsertNextPoint(phs);
+	m_hs_vertices->InsertNextCell(1,pid);
+	m_Append_hs->AddInput(m_hs_poly);
+	pid[0] = m_depth_points->InsertNextPoint(pdep);
+	m_depth_vertices->InsertNextCell(1,pid);
+	m_Append_depth->AddInput(m_depth_poly);
 }
 
 void DrawView::Render()
 {
-
+	m_Append_hs->Update();
+	m_Append_depth->Update();
+	m_RenderWindow->Render();
 }
-
 void DrawView::Clear()
 {
-	m_points.clear();
+	m_raw_points.clear();
+	vtkSmartNew_Initialize(m_hs_points);
+	vtkSmartNew_Initialize(m_depth_points);
+	vtkSmartNew_Initialize(m_hs_vertices);
+	vtkSmartNew_Initialize(m_depth_vertices);
+	m_hs_poly->SetPoints(m_hs_points);
+	m_depth_poly->SetPoints(m_depth_points);
+	m_hs_poly->SetVerts(m_hs_vertices);
+	m_depth_poly->SetVerts(m_depth_vertices);
+}
+
+void DrawView::SetHwnd( HWND hwnd )
+{
+	m_RenderWindow->SetParentId(hwnd);
+}
+
+void DrawView::ReSize( int w, int h )
+{
+	m_RenderWindow->SetSize(w, h);
+}
+
+void DrawView::AddTest()
+{
+	//vtkSmartNew_Initialize(m_hs_vertices);
+	//vtkSmartNew_Initialize(m_hs_points);
+	int i, j, k,  kOffset, jOffset, offset;
+	const int x_len = 5,
+		y_len = 5,
+		z_len = 5;
+	for(k=0;k<z_len;k++)
+	{
+		kOffset = k*y_len*x_len;
+		for(j=0; j<y_len; j++)
+		{
+			jOffset = j*x_len;
+			for(i=0;i<x_len;i++)
+			{
+				offset = i + jOffset + kOffset;
+				const float p[3] = {i, j, k};
+				vtkIdType pid[1];
+				pid[0] = m_hs_points->InsertNextPoint(i, j, k);
+				m_hs_vertices->InsertNextCell(1,pid);
+			}
+		}
+	}
+	m_Append_hs->AddInput(m_hs_poly);
+}
+
+void DrawView::FocusLast()
+{
+	if (!m_raw_points.empty())
+	{
+		DataPoint &data = m_raw_points.back();
+		m_Camera->SetPosition(data.E, data.N, m_focus_height/10.0);
+		double pos[3];
+		m_Camera->GetPosition(pos);
+		m_Camera->SetFocalPoint(pos[0], pos[1], pos[2]-1);
+		m_Camera->SetFocalDisk(data.depth+1);
+		m_Camera->UpdateViewport(m_Renderer);
+	}
+}
+
+void DrawView::NormalLook( double angle )
+{
+	double pos[3];
+	m_Camera->SetViewUp(0, 1, 0);
+	m_Camera->GetPosition(pos);
+	m_Camera->SetPosition(pos[0], pos[1], m_focus_height/10.0);
+	m_Camera->SetFocalPoint(pos[0], pos[1], pos[2]-1);
+	m_Camera->SetFocalDisk(pos[2]+1);
 }
 
--- fishfinding/mainframe.h
+++ fishfinding/mainframe.h
@@ -14,123 +14,166 @@
 
 
 /*!
- * Includes
- */
+* Includes
+*/
 
 ////@begin includes
+#include "wx/aui/framemanager.h"
 #include "wx/frame.h"
 #include "wx/gbsizer.h"
+#include "wx/spinctrl.h"
 #include "wx/filepicker.h"
+#include "wx/clrpicker.h"
 #include "wx/glcanvas.h"
 ////@end includes
 #include "NmeaCell.h"
+#include "DrawView.h"
 /*!
- * Forward declarations
- */
+* Forward declarations
+*/
 
 ////@begin forward declarations
+class wxSpinCtrl;
 class wxFilePickerCtrl;
+class wxColourPickerCtrl;
 class wxGLCanvas;
 ////@end forward declarations
 
 /*!
- * Control identifiers
- */
+* Control identifiers
+*/
 
 ////@begin control identifiers
 #define ID_MAINFRAME 10000
+#define ID_PANEL 10006
 #define ID_CHOICE1 10002
 #define ID_CHOICE 10001
 #define ID_BUTTON 10003
-#define ID_TEXTCTRL 10008
 #define ID_CHECKBOX 10005
+#define ID_CHECKBOX2 10012
+#define ID_SPINCTRL 10011
+#define ID_CHECKBOX1 10010
 #define ID_BUTTON1 10004
 #define ID_FILECTRL 10007
+#define ID_COLOURCTRL 10013
+#define ID_COLOURPICKERCTRL 10014
 #define ID_GLCANVAS 10009
-#define SYMBOL_MAINFRAME_STYLE wxCAPTION|wxRESIZE_BORDER|wxSYSTEM_MENU|wxCLOSE_BOX
+#define ID_TEXTCTRL 10008
+#define SYMBOL_MAINFRAME_STYLE wxCAPTION|wxRESIZE_BORDER|wxSYSTEM_MENU|wxMINIMIZE_BOX|wxMAXIMIZE_BOX|wxCLOSE_BOX
 #define SYMBOL_MAINFRAME_TITLE _("fishfinding")
 #define SYMBOL_MAINFRAME_IDNAME ID_MAINFRAME
 #define SYMBOL_MAINFRAME_SIZE wxSize(800, 600)
 #define SYMBOL_MAINFRAME_POSITION wxDefaultPosition
 ////@end control identifiers
 
-
+enum
+{
+	DRAW_TIMER1 = 1
+};
 /*!
- * mainframe class declaration
- */
+* mainframe class declaration
+*/
 
 class mainframe: public wxFrame
 {    
-    DECLARE_CLASS( mainframe )
-    DECLARE_EVENT_TABLE()
+	DECLARE_CLASS( mainframe )
+	DECLARE_EVENT_TABLE()
 
 public:
-    /// Constructors
-    mainframe();
-    mainframe( wxWindow* parent, wxWindowID id = SYMBOL_MAINFRAME_IDNAME, const wxString& caption = SYMBOL_MAINFRAME_TITLE, const wxPoint& pos = SYMBOL_MAINFRAME_POSITION, const wxSize& size = SYMBOL_MAINFRAME_SIZE, long style = SYMBOL_MAINFRAME_STYLE );
+	/// Constructors
+	mainframe();
+	mainframe( wxWindow* parent, wxWindowID id = SYMBOL_MAINFRAME_IDNAME, const wxString& caption = SYMBOL_MAINFRAME_TITLE, const wxPoint& pos = SYMBOL_MAINFRAME_POSITION, const wxSize& size = SYMBOL_MAINFRAME_SIZE, long style = SYMBOL_MAINFRAME_STYLE );
 
-    bool Create( wxWindow* parent, wxWindowID id = SYMBOL_MAINFRAME_IDNAME, const wxString& caption = SYMBOL_MAINFRAME_TITLE, const wxPoint& pos = SYMBOL_MAINFRAME_POSITION, const wxSize& size = SYMBOL_MAINFRAME_SIZE, long style = SYMBOL_MAINFRAME_STYLE );
+	bool Create( wxWindow* parent, wxWindowID id = SYMBOL_MAINFRAME_IDNAME, const wxString& caption = SYMBOL_MAINFRAME_TITLE, const wxPoint& pos = SYMBOL_MAINFRAME_POSITION, const wxSize& size = SYMBOL_MAINFRAME_SIZE, long style = SYMBOL_MAINFRAME_STYLE );
 
-    /// Destructor
-    ~mainframe();
+	/// Destructor
+	~mainframe();
 
-    /// Initialises member variables
-    void Init();
+	/// Initialises member variables
+	void Init();
 
-    /// Creates the controls and sizers
-    void CreateControls();
+	/// Creates the controls and sizers
+	void CreateControls();
 
-////@begin mainframe event handler declarations
+	////@begin mainframe event handler declarations
 
-    /// wxEVT_COMMAND_CHOICE_SELECTED event handler for ID_CHOICE1
-    void OnChoice1Selected( wxCommandEvent& event );
+	/// wxEVT_COMMAND_CHOICE_SELECTED event handler for ID_CHOICE1
+	void OnChoice1Selected( wxCommandEvent& event );
 
-    /// wxEVT_COMMAND_CHOICE_SELECTED event handler for ID_CHOICE
-    void OnChoiceSelected( wxCommandEvent& event );
+	/// wxEVT_COMMAND_CHOICE_SELECTED event handler for ID_CHOICE
+	void OnChoiceSelected( wxCommandEvent& event );
 
-    /// wxEVT_COMMAND_BUTTON_CLICKED event handler for ID_BUTTON
-    void OnStartGetClick( wxCommandEvent& event );
+	/// wxEVT_COMMAND_BUTTON_CLICKED event handler for ID_BUTTON
+	void OnStartGetClick( wxCommandEvent& event );
 
-    /// wxEVT_COMMAND_CHECKBOX_CLICKED event handler for ID_CHECKBOX
-    void OnCheckboxClick( wxCommandEvent& event );
+	/// wxEVT_COMMAND_CHECKBOX_CLICKED event handler for ID_CHECKBOX
+	void OnCheckboxClick( wxCommandEvent& event );
 
-    /// wxEVT_COMMAND_BUTTON_CLICKED event handler for ID_BUTTON1
-    void OnStopGetClick( wxCommandEvent& event );
+	/// wxEVT_COMMAND_CHECKBOX_CLICKED event handler for ID_CHECKBOX2
+	void OnCheckbox2Click( wxCommandEvent& event );
 
-    /// wxEVT_FILEPICKER_CHANGED event handler for ID_FILECTRL
-    void OnFilectrlFilePickerChanged( wxFileDirPickerEvent& event );
+	/// wxEVT_COMMAND_SPINCTRL_UPDATED event handler for ID_SPINCTRL
+	void OnSpinctrlUpdated( wxSpinEvent& event );
 
-////@end mainframe event handler declarations
+	/// wxEVT_COMMAND_BUTTON_CLICKED event handler for ID_BUTTON1
+	void OnStopGetClick( wxCommandEvent& event );
 
-////@begin mainframe member function declarations
+	/// wxEVT_FILEPICKER_CHANGED event handler for ID_FILECTRL
+	void OnFilectrlFilePickerChanged( wxFileDirPickerEvent& event );
 
-    /// Retrieves bitmap resources
-    wxBitmap GetBitmapResource( const wxString& name );
+	/// wxEVT_COLOURPICKER_CHANGED event handler for ID_COLOURCTRL
+	void OnDeepColorChanged( wxColourPickerEvent& event );
 
-    /// Retrieves icon resources
-    wxIcon GetIconResource( const wxString& name );
-////@end mainframe member function declarations
+	/// wxEVT_COLOURPICKER_CHANGED event handler for ID_COLOURPICKERCTRL
+	void OnhsColorChanged( wxColourPickerEvent& event );
 
-    /// Should we show tooltips?
-    static bool ShowToolTips();
+	/// wxEVT_SIZE event handler for ID_GLCANVAS
+	void OnCanvasSize( wxSizeEvent& event );
 
-////@begin mainframe member variables
-    wxChoice* m_BoundRate;
-    wxChoice* m_Combo_ComPort;
-    wxButton* m_BtnStartGet;
-    wxTextCtrl* m_OutputText;
-    wxButton* m_BtnStopGet;
-    wxFilePickerCtrl* m_Browse;
-    wxStaticText* m_WaterDepth;
-    wxStaticText* m_Longitude;
-    wxStaticText* m_Latitude;
-    wxStaticText* m_DataTotal;
-    wxGLCanvas* m_GLCanvas;
-////@end mainframe member variables
-    long m_port;
-    bool m_open;
-    NmeaCell m_nCell;
+	/// wxEVT_PAINT event handler for ID_GLCANVAS
+	void OnPaint( wxPaintEvent& event );
+
+	////@end mainframe event handler declarations
+
+	////@begin mainframe member function declarations
+
+	/// Returns the AUI manager object
+	wxAuiManager& GetAuiManager() { return m_auiManager; }
+
+	/// Retrieves bitmap resources
+	wxBitmap GetBitmapResource( const wxString& name );
+
+	/// Retrieves icon resources
+	wxIcon GetIconResource( const wxString& name );
+	////@end mainframe member function declarations
+
+	/// Should we show tooltips?
+	static bool ShowToolTips();
+	void RenderFrame();
+	////@begin mainframe member variables
+	wxAuiManager m_auiManager;
+	wxChoice* m_BoundRate;
+	wxChoice* m_Combo_ComPort;
+	wxButton* m_BtnStartGet;
+	wxCheckBox* m_NormalLook;
+	wxSpinCtrl* m_spinctrl_height;
+	wxCheckBox* m_FocusLast;
+	wxButton* m_BtnStopGet;
+	wxFilePickerCtrl* m_Browse;
+	wxStaticText* m_WaterDepth;
+	wxStaticText* m_Longitude;
+	wxStaticText* m_Latitude;
+	wxStaticText* m_DataTotal;
+	wxColourPickerCtrl* m_DeepColor;
+	wxColourPickerCtrl* m_hsColor;
+	wxGLCanvas* m_GLCanvas;
+	wxTextCtrl* m_OutputText;
+	////@end mainframe member variables
+	long m_port;
+	bool m_open, m_timer_go;
+	NmeaCell m_nCell;
+	DrawView m_DrawView;
 };
 
 #endif
-    // _MAINFRAME_H_
+// _MAINFRAME_H_
